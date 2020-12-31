3d4ffcc72d63963ee29c4187b57e55595add4312
davisantosr <davi-rocha2@hotmail.com>
2020-12-31 16:30:59 -0300

    add Home container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import 'bootswatch/dist/cerulean/bootstrap.min.css';
 
+import Home from './containers/Home'
+
 import Navbar from './components/Navbar'
 
 
 function App() {
   return (
-    <Navbar />
+    <div className='container' >
+      <Navbar />
+      <Home />
+    </div>
   )
 }
 
---
+++ src/containers/Home.js
@@ -0,0 +1,19 @@
+
+
+const home = () => {
+  return (
+    <div class="jumbotron">
+      <h1 class="display-3">Bem Vindo</h1>
+      <p 
+      class="lead">Esse é o sistema</p>
+      <hr class="my-4"/>
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Cadastrar</a>
+      </p>
+    </div>
+
+  )
+}
+
+export default home;
