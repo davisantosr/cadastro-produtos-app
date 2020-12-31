4ae3543904c6058e2e58b06bebfe5061fffb019a
davisantosr <davi-rocha2@hotmail.com>
2020-12-31 13:58:28 -0300

    add bootswatch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 
+import 'bootswatch/dist/cerulean/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
+      <div className="alert alert-dismissible alert-warning">
+        <button type="button" className="close" data-dismiss="alert">&times;</button>
+        <h4 className="alert-heading">Warning!</h4>
+        <p className="mb-0">Hello<a href="#" className="alert-link">bootswatch</a>.</p>
+      </div>
    
     </div>
   );
